d61901afe57a5fcf6ff04c2beaad413753742af9
EL IDRISSI MOHAMMED <ingmedical16@gmail.com>
2023-10-16 01:18:46 +0200

    List Products

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 3. Create Vite App
 
 4. Create Git Repository
+
+5. List Products
+    i- create products array
+    ii- add product images
+    iii- render products
+    iv- style products
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
+import data from './data/data';
+
 function App() {
   return (
     <>
       <header>
-        <a href="/">amazona</a>
+        <a href="/">E-Shop</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product, i) => (
+            <div className="product" key={i}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+
+                <p><strong>${product.price}</strong></p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </>
   );
 }
