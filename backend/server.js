33f9972be595f67edb5be59f9c9982792d070907
EL IDRISSI MOHAMMED <ingmedical16@gmail.com>
2023-10-16 23:56:08 +0200

    19. Connect To MongoDB Database

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 import express from 'express';
 import data from './data/data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 const app = express();
 
 app.get('/api/V2/products', (req, res) => {
